dca4b5304e3f3a15db5442eeba942419fa5110c8
Shadrach Almine <shadrachalmine53@gmail.com>
2026-04-08 17:23:34 +0800

    home

---
+++ my-portfolio/src/app/components/home/home.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.html',
+  styleUrls: []
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  typedText = '';
+
+  private words    = ['Computer Engineer', 'UI/UX Designer', 'Frontend Engineer', 'Problem Solver'];
+  private wIndex   = 0;
+  private cIndex   = 0;
+  private deleting = false;
+  private typingTimer: any;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.startTyping();
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.typingTimer);
+  }
+
+  private startTyping(): void {
+    const current = this.words[this.wIndex];
+
+    if (!this.deleting) {
+      this.typedText = current.substring(0, this.cIndex + 1);
+      this.cIndex++;
+      this.cdr.detectChanges();
+
+      if (this.cIndex === current.length) {
+        this.typingTimer = setTimeout(() => {
+          this.deleting = true;
+          this.startTyping();
+        }, 2000);
+        return;
+      }
+
+    } else {
+      this.typedText = current.substring(0, this.cIndex - 1);
+      this.cIndex--;
+      this.cdr.detectChanges();
+
+      if (this.cIndex === 0) {
+        this.deleting = false;
+        this.wIndex   = (this.wIndex + 1) % this.words.length;
+      }
+    }
+
+    const speed = this.deleting ? 60 : 100;
+    this.typingTimer = setTimeout(() => this.startTyping(), speed);
+  }
+}
---
+++ my-portfolio/src/app/components/navbar/navbar.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrls: []
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+  }
+}
